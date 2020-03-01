6069d9b6f8db925b26bf6f257845df9ec3b2d05c
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-01 22:10:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,13 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @student_hash = student_hash
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end 
   end
 
   def add_student_attributes(attributes_hash)
